647e7de86d09305ef946f9310d5cbfd8af0e9851
Michael Robards <mikerobards@icloud.com>
2021-11-03 16:36:28 -0400

    add cake

--- src/App.js
+++ src/App.js
@@ -1,9 +1,71 @@
-function App() {
+import React, { useState, useReducer } from 'react';
+import ReactDOM from 'react-dom';
+
+// let arr = [1, 2, 3, 4, 5]
+// let add = (x, y) => x + y
+// let sum = arr.reduce(add, 0)
+
+// console.log(sum) // 15: 0 + 1 + 2 + 3 + 4 + 5
+
+
+let initialState = {
+  count: 0
+}
+
+let actions = [
+  { type: 'ADD', by: 2 },
+  { type: 'MINUS', by: 4 },
+  { type: 'ADD', by: 10 },
+  { type: 'EAT_CAKE' }
+]
+
+function reducer(state, action) {
+
+  switch (action.type) {
+    case 'ADD': {
+      return { ...state, count: state.count + action.by }
+    }
+    case 'MINUS': {
+      return { ...state, count: state.count - action.by }
+    }
+    case 'EAT_CAKE': {
+      return { ...state, cake: false }
+    }
+    default: {
+      return state
+    }
+  }
+}
+
+
+
+console.log(actions.reduce(reducer, initialState))
+
+
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  const add = () => {
+    setCount(count + 1)
+  }
+
+  const subtract = () => {
+    if (count > 0) {
+      setCount(count - 1)
+    }
+  }
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
+    <section>
+      <h2>Counter: The Most Novel Example I Could Come Up With</h2>
+      <div className="counter">
+        <button onClick={subtract}>-</button>
+        <div>{count}</div>
+        <button onClick={add}>+</button>
+      </div>
+    </section>
+  )
 }
 
 export default App;
